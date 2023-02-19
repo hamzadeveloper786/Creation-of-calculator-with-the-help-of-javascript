7e3b4ca8694cc92a8f4688b12004843cfdf48c21
Johnprocessor <HaCkErHaMzA@JoHnHaMzA>
2023-02-19 22:05:22 +0400

    Creation of calculator commit

---
+++ add.mjs
@@ -0,0 +1,9 @@
+let num1 =prompt("Enter your first value: ")
+let num2 =prompt("Enter your seccond number: ")
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+let num3 =num1 + num2;
+
+alert("Sum is: " + num3)
---
+++ mul.mjs
@@ -0,0 +1,16 @@
+let num1 = prompt(" Enter your first number: ")
+
+console.log (num1);
+
+let num2 = prompt(" Enter your second number: ")
+ 
+console.log (num2);
+
+num1 = Number(num1)
+num2 = Number(num2)
+
+ let num3 =num1 * num2 
+
+ console.log (num3);
+ 
+ alert("Product is: " + num3 )
